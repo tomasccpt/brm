227c0e44009a4a7ec9048e23ca18ef3fe1524715
Miguel Borges <up202004481@edu.fe.up.pt>
2024-04-18 19:00:09 +0100

    add uc code

---
+++ uc.ino
@@ -0,0 +1,53 @@
+#include <Servo.h>
+
+#define len(x) (sizeof(x) / sizeof(*(x)))
+#define clamp(min, x, max) ((x)<(min)?(min):(x)>(max)?(max):x)
+
+Servo Servos[4];
+int ReadVal[4] = { 0 };
+int PastVal[4] = { 0 };
+int Ports[4] = { 4, 5, 6, 7 };
+int MinAngle[4] = { 10, 10, 10, 100 };
+int MaxAngle[4] = { 170, 170, 170, 180 };
+
+void setup() {
+  Serial.begin(9600);
+
+  for (size_t i = 0; i < len(Servos); ++i) {
+    Servos[i].attach(Ports[i]);
+  }
+  Servos[0].attach(4);
+  Servos[1].attach(5);
+  Servos[2].attach(6);
+  Servos[3].attach(7);
+  pinMode(3, INPUT);
+}
+
+void loop() {
+  char readBuf[sizeof(unsigned char)*len(ReadVal)];
+  size_t read = Serial.readBytes(readBuf, sizeof(readBuf));
+  bool unsynced = false;
+
+  if (read) {
+    unsynced = true;
+    for (size_t i = 0; i < len(ReadVal); ++i) {
+      ReadVal[i] = (int)*(unsigned char*)(readBuf + i);
+      ReadVal[i] = clamp(MinAngle[i], ReadVal[i], MaxAngle[i]);
+    }
+  }
+  
+  while (unsynced) {
+    unsynced = false;
+    for (size_t i = 0; i < len(Servos); ++i) {
+      if ((ReadVal[i] - PastVal[i]) > 0) {
+        PastVal[i] += 1;
+        Servos[i].write(PastVal[i]);
+        unsynced = true;
+      } else if ((ReadVal[i] - PastVal[i]) < 0) {
+        PastVal[i] -= 1;
+        Servos[i].write(PastVal[i]);
+        unsynced = true;
+      }
+    }
+  }
+}
