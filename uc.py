2863b343f30f94eded75c30a3ec5745cde69cdf8
José Teixeira <up202006243@up.pt>
2024-04-19 16:04:09 +0100

    Allow run without robot

--- uc.py
+++ uc.py
@@ -1,27 +1,31 @@
 import serial
 import time
 
-ser = serial.Serial('COM3', 9600)
-last_timestamp = 0
+ROBOT = False
+if ROBOT:
+    ser = serial.Serial('COM3', 9600)
+    last_timestamp = 0
 
-def send(values):
-    """
-    global last_timestamp
-    # Works as long as each list value is a single byte (-127 <= 0 <= 255).
-    while True:
-        t = time.perf_counter_ns()
-        if t - last_timestamp > 0.8e6:
-            break
-        time.sleep(0)
-    ser.write(bytes(values))
-    #ser.flush()
-    last_timestamp = time.perf_counter_ns()
-    """
-    while True:
-        try:
-            ser.write(bytes(values))
-            break
-        except:
-            time.sleep(0.1)        
-    
-    
+    def send(values):
+        """
+        global last_timestamp
+        # Works as long as each list value is a single byte (-127 <= 0 <= 255).
+        while True:
+            t = time.perf_counter_ns()
+            if t - last_timestamp > 0.8e6:
+                break
+            time.sleep(0)
+        ser.write(bytes(values))
+        #ser.flush()
+        last_timestamp = time.perf_counter_ns()
+        """
+        while True:
+            try:
+                ser.write(bytes(values))
+                break
+            except:
+                time.sleep(0.1)
+else:
+    def send(values):
+        print(values)
+        time.sleep(0.1)
